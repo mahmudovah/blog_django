4a99b47f9361b2b411c62fd78a93b7b0e93eeaae
mahmudovah <mahmudovahadija.0831@gmail.com>
2026-02-18 07:19:52 +0500

    urlda todo sahifaga yozish qo'shildi

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Todo
 
-# Create your views here.
+
+def todo_list(request):
+    if request.method == "POST":
+        body = request.POST.get("body")
+        duration = request.POST.get("duration")
+        duration_type = request.POST.get("duration_type")
+        status = request.POST.get("status")
+
+        todo = Todo.objects.create(
+            body = body,
+            duration = duration,
+            duration_type = duration_type,
+            status = status
+        )
+        return redirect("todo_list")
+    
+    todos = Todo.objects.all()
+    
+    return render(request,"todos.html", context={"todos":todos})
